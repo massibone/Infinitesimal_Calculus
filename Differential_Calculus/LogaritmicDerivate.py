363a01b61c092197ba8f9f77a8b077034433fe4d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-06-03 10:10:50 +0200

    Create LogaritmicDerivate.py

---
+++ Differential_Calculus/LogaritmicDerivate.py
@@ -0,0 +1,16 @@
+import sympy as sp
+# Definizione della variabile e della funzione
+
+x = sp.Symbol('x')
+
+f = sp.log(x)
+
+# Calcolo della derivata logaritmica (elasticità)
+
+elasticita = sp.diff(f, x) / f
+
+# Stampa la derivata logaritmica (elasticità)
+
+print(f"Derivata logaritmica (elasticità) di f(x) = log(x):")
+
+print(elasticita)
