23e09b7241884bf1e92fc1a573a9cbb4d8314287
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-07-10 08:07:45 +0200

    Add files via upload

---
+++ Integral_Calculus/dataFitting.py
@@ -0,0 +1,46 @@
+import numpy as np
+import csv
+import matplotlib.pylab as plt
+from scipy.optimize import curve_fit
+from scipy import integrate as intg
+
+#Import CSV Data
+with open("Data.csv",'r') as i:           
+    rawdata = list(csv.reader(i,delimiter=","))   
+
+exampledata = np.array(rawdata[1:],dtype=np.cfloat)    #convert to data array
+xdata = exampledata[:,0]
+ydata = exampledata[:,1]
+
+#Plot the Data 
+plt.figure(1,dpi=120)
+plt.yscale('linear')
+plt.xscale('linear')
+
+
+plt.title("Example Data")
+plt.xlabel(rawdata[0][0])
+plt.ylabel(rawdata[0][1])
+plt.plot(xdata,ydata,label="Experimental Data")
+
+#Define Function
+def func(x,b):                                   #input x in nm and b in nm^-1
+    return a0 * np.exp(-b * x) + a1
+
+#Define Constants
+a0 = 2.5    #W m^-2 nm^-1
+a1 = 0.5    #W m^-2 nm^-1
+
+funcdata = func(xdata,1.375)                 #Generate & Plot data for comparison
+plt.plot(xdata,funcdata,label="Model")   
+plt.legend()
+plt.show()
+#Curve fit data to model
+popt, pcov = curve_fit(func,xdata,ydata,bounds=(0,4))
+perr = np.sqrt(np.diag(pcov))
+
+#Integrals
+TotalInt = intg.trapz(ydata,xdata)                     #Compute numerical integral
+TotalInt_func = intg.quad(func,0,4, args=(1.375))[0]        #Compute integral of function
+low_Frac = intg.quad(func,0,2, args=(1.375))[0]/TotalInt_func
+high_Frac = intg.quad(func,2,4, args=(1.375))[0]/TotalInt_func
