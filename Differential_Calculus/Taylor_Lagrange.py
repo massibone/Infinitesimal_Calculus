949dc196a7c61a975d0bdf4af80279bc3656a9fb
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-07-29 09:36:46 +0200

    Create Taylor_Lagrange.py

---
+++ Differential_Calculus/Taylor_Lagrange.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def taylor_lagrange(function, x0, n, interval):
+    """
+    Calcola e restituisce l'approssimazione di Taylor della funzione data con il resto secondo Lagrange.
+    
