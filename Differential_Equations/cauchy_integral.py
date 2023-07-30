399a1d44527b73cad3bc4044282d2c92a21b9694
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-07-30 10:29:16 +0200

    Update cauchy_integral.py

--- Differential_Equations/cauchy_integral.py
+++ Differential_Equations/cauchy_integral.py
@@ -1,1 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Funzione per calcolare l'integrale complesso
+def cauchy_integral(z, f, radius, num_points=500):
+    theta = np.linspace(0, 2 * np.pi, num_points)
+    points = z + radius * np.exp(1j * theta)
+    values = f(points)
+    integral = np.sum(values) * (2 * np.pi * radius) / num_points
+    return integral
