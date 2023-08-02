525b1f67045bde1086e86aefc163e89134ee4b5c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-02 08:00:59 +0200

    Add files via upload

---
+++ Differential_Equations/separable_equation.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definizione dell'equazione differenziale
+def separable_equation(y, x):
+    return y**2 - x**2
+
+# Definizione del dominio x
+x = np.linspace(-5, 5, 100)
+
+# Risoluzione dell'equazione differenziale
+y = np.sqrt((x**3)/3 + 5)   # Soluzione dell'equazione differenziale (ottenuta analiticamente)
+
+# Tracciamento della soluzione
+plt.plot(x, y, label='Soluzione')
+
+# Configurazione del grafico
+plt.title("Equazione differenziale a variabili separabili")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+
+# Mostra il grafico
+plt.show()
