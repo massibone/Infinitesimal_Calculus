e64b25801d6652520f51ae4eb8561204f4a53847
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-28 07:14:10 +0300

    Update first_second_order.py

--- Differential_Equations/first_second_order.py
+++ Differential_Equations/first_second_order.py
@@ -8,3 +8,11 @@ def first_order_eq(y, t):
 def second_order_eq(y, t):
     return -0.5 * y - 2 * np.sin(t)
 
+# Definizione del dominio t
+t = np.linspace(0, 10, 100)
+
+# Risoluzione dell'equazione lineare del primo ordine
+y_first_order = np.exp(-0.5 * t)
+
+# Risoluzione dell'equazione lineare del secondo ordine
+y_second_order = np.exp(-0.5 * t) + np.sin(t)
