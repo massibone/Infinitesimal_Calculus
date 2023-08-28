8c68de40251b8e3b6e28bc3a020578a0019de7c3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-28 07:13:30 +0300

    Update first_second_order.py

--- Differential_Equations/first_second_order.py
+++ Differential_Equations/first_second_order.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# Definizione dell'equazione lineare del primo ordine
+def first_order_eq(y, t):
+    return -0.5 * y
+
+# Definizione dell'equazione lineare del secondo ordine
+def second_order_eq(y, t):
+    return -0.5 * y - 2 * np.sin(t)
+
