0659dd509418777e7b548f577050a7eb980c6790
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-29 06:03:45 +0300

    Update first_second_order.py

--- Differential_Equations/first_second_order.py
+++ Differential_Equations/first_second_order.py
@@ -16,3 +16,16 @@ y_first_order = np.exp(-0.5 * t)
 
 # Risoluzione dell'equazione lineare del secondo ordine
 y_second_order = np.exp(-0.5 * t) + np.sin(t)
+
+# Tracciamento delle soluzioni
+plt.plot(t, y_first_order, label='Primo ordine')
+plt.plot(t, y_second_order, label='Secondo ordine')
+
+# Configurazione del grafico
+plt.title("Equazioni lineari del primo e secondo ordine")
+plt.xlabel("Tempo")
+plt.ylabel("y(t)")
+plt.legend()
+
+# Mostra il grafico
+plt.show()
