54d20466bedf5848ff2acbb6415725df41adb572
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-30 08:31:27 +0200

    Create IntegralTheorem.py

---
+++ Integral_Calculus/IntegralTheorem.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def integral_function(x):
+    # Define the function for integration here
+    return x ** 2  # Example function: x^2
+
+def derivative_function(x):
+    # Define the derivative of the function here
+    return 2 * x  # Example derivative: 2x
+
+# Define the interval for calculation
+a = 0  # Lower limit
+b = 2  # Upper limit
