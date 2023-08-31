996c60b4516bd89b6a4e1c6ec7a29cb654deaadc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-31 10:04:20 +0200

    Update IntegralTheorem.py

--- Integral_Calculus/IntegralTheorem.py
+++ Integral_Calculus/IntegralTheorem.py
@@ -12,3 +12,32 @@ def derivative_function(x):
 # Define the interval for calculation
 a = 0  # Lower limit
 b = 2  # Upper limit
+
+# Calculate the definite integral using the integral function
+definite_integral = np.linspace(a, b, 100)
+integral_values = [integral_function(x) for x in definite_integral]
+
+# Calculate the antiderivative (indefinite integral) using the derivative function
+indefinite_integral = np.linspace(a, b, 100)
+antiderivative_values = [derivative_function(x) for x in indefinite_integral]
+
+# Plot the definite integral and antiderivative
+plt.figure(figsize=(10, 6))
+
+# Plot the definite integral
+plt.subplot(2, 1, 1)
+plt.plot(definite_integral, integral_values, 'b-', linewidth=2)
+plt.fill_between(definite_integral, 0, integral_values, alpha=0.3, color='blue')
+plt.title('Definite Integral')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Plot the antiderivative (indefinite integral)
+plt.subplot(2, 1, 2)
+plt.plot(indefinite_integral, antiderivative_values, 'r-', linewidth=2)
+plt.title('Antiderivative (Indefinite Integral)')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.tight_layout()
+plt.show()
