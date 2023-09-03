1a02fa9cf13ab9d0de52291ad58c8c6d3805ffd0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-03 10:50:43 +0200

    Update TaylorSeries2.py

--- Differential_Calculus/TaylorSeries2.py
+++ Differential_Calculus/TaylorSeries2.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def taylor_series(function, x0, n, interval):
+    """
+    Calcola e restituisce l'approssimazione di Taylor della funzione data.
+
+    Parametri:
+    - function: la funzione da approssimare
+    - x0: il punto di espansione
+    - n: il grado massimo dell'approssimazione
+    - interval: l'intervallo su cui calcolare l'approssimazione
+    
