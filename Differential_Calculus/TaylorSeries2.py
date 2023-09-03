82f37bad8cdffbe9e14768ad29654459b2855b93
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-03 10:51:19 +0200

    Update TaylorSeries2.py

--- Differential_Calculus/TaylorSeries2.py
+++ Differential_Calculus/TaylorSeries2.py
@@ -11,3 +11,21 @@ def taylor_series(function, x0, n, interval):
     - n: il grado massimo dell'approssimazione
     - interval: l'intervallo su cui calcolare l'approssimazione
     
+Restituisce:
+    - x_vals: i valori dell'ascissa
+    - y_vals: i valori dell'approssimazione di Taylor
+    """
+
+    x_vals = np.linspace(interval[0], interval[1], 1000)
+    y_vals = np.zeros_like(x_vals)
+
+    for i, x in enumerate(x_vals):
+        y = 0.0
+
+        for k in range(n + 1):
+            # Calcola l'approssimazione di Taylor al grado k
+            y += function(x0, k) * (x - x0) ** k / np.math.factorial(k)
+
+        y_vals[i] = y
+
+    return x_vals, y_vals
