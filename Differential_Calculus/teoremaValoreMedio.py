3e4b238199c2fcf23b260a0ae854763b6a0a414b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-07 10:58:37 +0200

    Create teoremaValoreMedio.py

---
+++ Differential_Calculus/teoremaValoreMedio.py
@@ -0,0 +1,11 @@
+'''
+Il Teorema del Valor Medio (TVM) delle derivate afferma quanto segue:
+
+Supponiamo che una funzione f(x) sia continua sull'intervallo chiuso [a, b] 
+e derivabile sull'intervallo aperto (a, b).
+Allora esiste almeno un punto c in (a, b) tale che 
+la derivata istantanea f'(c) della funzione nel punto c è uguale 
+al rapporto incrementale medio della funzione sull'intervallo [a, b].
+
+'''
+
