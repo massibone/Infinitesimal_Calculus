df24f78cc61b8325c5c422da1de9c36f25511ea8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-07 10:59:20 +0200

    Update teoremaValoreMedio.py

--- Differential_Calculus/teoremaValoreMedio.py
+++ Differential_Calculus/teoremaValoreMedio.py
@@ -9,3 +9,28 @@ al rapporto incrementale medio della funzione sull'intervallo [a, b].
 
 '''
 
+import sympy as sp
+
+# Definizione della variabile e delle funzioni
+x = sp.Symbol('x')
+f = sp.Function('f')(x)
+
+# Definizione dell'intervallo [a, b]
+a = 0
+b = 2
+
+# Calcolo della derivata di f
+df = sp.diff(f, x)
+
+# Calcolo del valore medio di f sull'intervallo [a, b]
+valor_medio = (f.subs(x, b) - f.subs(x, a)) / (b - a)
+
+# Verifica delle conseguenze del Teorema del Valor Medio
+
+# Conseguenza 1: Se f è continua sull'intervallo [a, b] e derivabile su (a, b),
+# allora esiste almeno un punto c in (a, b) tale che f'(c) = valor_medio
+conseguenza_1 = sp.solveset(df - valor_medio, x, domain=sp.Interval.open(a, b))
+
+# Conseguenza 2: Se f'(x) > 0 per ogni x in (a, b), allora f è strettamente crescente sull'intervallo [a, b]
+conseguenza_2 = df > 0
+
