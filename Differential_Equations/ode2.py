f3a855743d77418eab600d8823daa8d5b4288045
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-14 10:04:22 +0200

    Update ode2.py

--- Differential_Equations/ode2.py
+++ Differential_Equations/ode2.py
@@ -1,1 +1,10 @@
+import numpy as np
+import scipy.integrate as intg
+import matplotlib.pyplot as plt
 
+sigma=5.67e-8
+qgen=400000 #more for parable function
+k=40 # m**-1 K**-1
+T1=0
+epsilon=0.8
+Tsur=500   #+273.15 #convert C to K
