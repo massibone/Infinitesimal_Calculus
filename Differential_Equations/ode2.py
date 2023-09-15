f4e81d13048efe979ad45a244783c30232f36a22
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-15 08:04:45 +0200

    Update ode2.py

--- Differential_Equations/ode2.py
+++ Differential_Equations/ode2.py
@@ -8,3 +8,20 @@ k=40 # m**-1 K**-1
 T1=0
 epsilon=0.8
 Tsur=500   #+273.15 #convert C to K
+L=1
+nodes=50
+x=np.linspace(0,L,nodes)
+
+def f(x,y):
+    return np.vstack((y[1],np.full_like(x,-qgen/k)))
+
+def bc(ya,yb):
+    return np.array([ya[0]-T1,k*yb[1]+sigma*epsilon*(yb[0]**4-Tsur**4)])
+
+y0=np.zeros((2,x.size))
+
+solution= intg.solve_bvp(f,bc,x,y0)
+T=solution.y[0]
+dTdx = solution.y[1]
+
+Ts1=T[0]
