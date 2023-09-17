ca9348f8a8bc0a2820c0fcdf9e746d0a1e67981e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-17 10:04:56 +0200

    Update Taylor_n&2.py

--- Differential_Calculus/Taylor_n&2.py
+++ Differential_Calculus/Taylor_n&2.py
@@ -1,1 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+def taylor_approximation(f, x, a, n):
+    approximation = f(a)
+    for i in range(1, n+1):
+        derivative = (f(a + 1e-6) - f(a)) / 1e-6  # Calcolo della derivata numerica
+        approximation += derivative / np.math.factorial(i) * (x-a)**i
+        f = lambda x: derivative  # Aggiornamento della funzione con la derivata calcolata
+    return approximation
