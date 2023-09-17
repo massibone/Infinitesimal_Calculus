1beb13f1b20d5e7c1ba13bac38026ff7e7569ab9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-17 10:05:30 +0200

    Update Taylor_n&2.py

--- Differential_Calculus/Taylor_n&2.py
+++ Differential_Calculus/Taylor_n&2.py
@@ -8,3 +8,13 @@ def taylor_approximation(f, x, a, n):
         approximation += derivative / np.math.factorial(i) * (x-a)**i
         f = lambda x: derivative  # Aggiornamento della funzione con la derivata calcolata
     return approximation
+def function(x):
+    return np.sin(x)
+
+x = np.linspace(-2*np.pi, 2*np.pi, 100)
+
+# Approssimazione di Taylor di ordine 4
+taylor_order_4 = taylor_approximation(function, x, 0, 4)
+
+# Approssimazione di Taylor di ordine 2
+taylor_order_2 = taylor_approximation(function, x, 0, 2)
